ccb4844aa5812782ca0659c1e32b636788ab0653
forwor <forwor@forwordeMacBook-Pro.local>
2018-11-21 10:27:48 +0800

    swift 4.1

--- Source/Controller/PhotosViewController.swift
+++ Source/Controller/PhotosViewController.swift
@@ -239,6 +239,7 @@ final class PhotosViewController : UICollectionViewController {
 
         // Enabled?
         doneBarButton?.isEnabled = photosDataSource.selections.count > 0
+        doneBarButton?.tintColor = UIColor.black
 
         navigationItem.rightBarButtonItem = doneBarButton
     }
